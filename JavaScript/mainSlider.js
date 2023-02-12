28b96b6d87e8ef35bd4ffcd9af730e5a065c131b
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-02-12 14:55:48 +0700

    Interior was added
    Interior was added

--- JavaScript/mainSlider.js
+++ JavaScript/mainSlider.js
@@ -10,95 +10,115 @@ const models = document.querySelectorAll('.model-item')
 const products = [
     {
       id: 1,
-      image: "./images/Camry-reward",
-      text: 'Признана лидером в сегменте STANDART (D) по сохранению остаточной стоимости в 2022г.',
+
+      text: 'Лидер в сегменте STANDART (D) по сохранению остаточной стоимости в 2022г.',
+      interiorImg: "./images/camry-interior.jpg",
       colors: [
         {
             code: "white",
+            name: "Белый",
             img: "./images/camry-color-white.png"
           },
         {
           code: "black",
+          name: "Черный",
           img: "./images/camry-color-black.png",
         },
         {
           code: "darkred",
+          name: "Красный",
           img: "./images/camry-color-darkred.png",
         },
       ],
     },
     {
       id: 2,
-      image: "./images/Rav4-reward",
-      text: 'Занял второе место в сегменте SUV (C) по сохранению остаточной стоимости в 2021г.',
+
+      text: 'Второе место в сегменте SUV (C) по сохранению остаточной стоимости в 2021г.',
+      interiorImg: "./images/rav4-interior.jpg",
       colors: [
         {
             code: "white",
+            name: "Белый",
             img: "./images/rav4-color-white.png"
           },
         {
           code: "black",
+          name: "Черный",
           img: "./images/rav4-color-black.png",
         },
         {
           code: "darkred",
+          name: "Красный",
           img: "./images/rav4-color-darkred.png",
         },
       ],
     },
     {
       id: 3,
-      image: "./images/Rav4-reward",
-      text: 'Одержал победу в номинации «Большие кроссоверы» престижной премии Гран-при «За рулем».',
+
+      text: 'Лидер сегмента SUV (D) по сохранению остаточной стоимости в 2018г.',
+      interiorImg: "./images/highlander-interior.jpg",
       colors: [
         {
             code: "white",
+            name: "Белый",
             img: "./images/highlander-color-whitepearl.png"
           },
         {
           code: "black",
+          name: "Черный",
           img: "./images/highlander-color-black.png",
         },
         {
           code: "darkred",
+          name: "Красный",
           img: "./images/highlander-color-darkred.png",
         },
       ],
     },
     {
       id: 4,
-      image: "./images/Rav4-reward",
-      text: 'Признан лидером в сегменте STANDART (Pickup) по сохранению остаточной стоимости в 2022г.',
+
+      text: 'Лидер в сегменте STANDART (Pickup) по сохранению остаточной стоимости в 2022г.',
+      interiorImg: "./images/hilux-interior.jpg",
       colors: [
         {
             code: "white",
+            name: "Белый",
             img: "./images/hilux-color-white.png"
           },
         {
           code: "black",
+          name: "Черный",
           img: "./images/hilux-color-black.png",
         },
         {
           code: "darkred",
+          name: "Красный",
           img: "./images/hilux-color-red.png",
         },
       ],
     },
     {
       id: 5,
-      image: "./images/Rav4-reward",
-      text: 'Занял третье место в сегменте STANDART (С) по сохранению остаточной стоимости в 2022г.',
+
+      text: 'Третье место в сегменте SUV (D) по сохранению остаточной стоимости в 2022г.',
+      interiorImg: "./images/prado-interior.jpg",
       colors: [
         {
             code: "white",
+            name: "Белый",
             img: "./images/prado-color-white.png"
           },
         {
           code: "black",
+          name: "Черный",
           img: "./images/prado-color-black.png",
         },
         {
           code: "grey",
+          name: "Серый",
           img: "./images/prado-color-grey.png",
         },
       ],
@@ -110,13 +130,17 @@ const products = [
 let choosenProduct = products[0]
 
 const currentProductImg = document.querySelector('.productImg')
-// const currentProductRewardImg = document.querySelector('.reward-image')
 const currentProductRewardText = document.querySelector('.rewardText')
 const currentProductColors = document.querySelectorAll('.color')
+const currentProductColorsNames = document.querySelectorAll('.colorName')
+const currentInteriorImg = document.querySelector('.interiorImg')
 
 
 
-// const currentColorNames = document.querySelectorAll('.color-name')
+// Необходимо найти решения для LC Prado по факту цвет серый, а текстом написано 'Красный', как у остальных
+// const currentColorNames = document.querySelectorAll('.colorName')
+
+
 
 // Код отвечает за работу слайдера
 
@@ -129,17 +153,34 @@ models.forEach((item, index) => {
 // Меняем выбранную модель
         choosenProduct = products[index]
         
+// Задаем каждой модели соответствующий текст номинации
         currentProductRewardText.textContent = choosenProduct.text;
 
-// Меняем картинку ТС
+
+// Это изобоажение будет первым для каждой модели
         currentProductImg.src = choosenProduct.colors[0].img
 
-// Меняем палитру цветов в соответствии с выбранной моделью
+// Это изображение интерьера каждого ТС
+        currentInteriorImg.src = choosenProduct.interiorImg
 
+// Меняем палитру цветов справа от картинки авто в соответствии с выбранной моделью
         currentProductColors.forEach((color, index) => {
             color.style.backgroundColor = choosenProduct.colors[index].code
         })
 
+// Меняем названия цветов, в данном примере актуально для Prado, так как вместо красного у него серый
+        currentProductColorsNames.forEach((name, index) => {
+            name.textContent = choosenProduct.colors[index].name
+        })
+
+
     })
 })
 
+
+// Меняем цвет по нажатию на соответствующую кнопку
+currentProductColors.forEach((color, index)=> {
+    color.addEventListener('click', () => {
+        currentProductImg.src = choosenProduct.colors[index].img;
+    })
+})
