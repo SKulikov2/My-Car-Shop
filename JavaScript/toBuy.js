f6c844f09f5f602ad3bbd66a6ce492a84f7eddd7
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-02-16 00:11:35 +0700

    Pay form was added
    Pay form was added

---
+++ JavaScript/toBuy.js
@@ -0,0 +1,14 @@
+// DOM элементы
+
+const buyButton = document.querySelector('.getCar')
+const payment = document.querySelector('.payment')
+const close = document.querySelector('.close')
+
+
+buyButton.addEventListener('click', () => {
+    payment.classList.remove('hidden')
+})
+
+close.addEventListener('click', () => {
+    payment.classList.add('hidden')
+})
