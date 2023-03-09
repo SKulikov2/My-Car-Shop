ac0ab8907863acb09aebc0b4c7715f58e33051e3
SKulikov2 <Kulikovsv.1987@mail.ru>
2023-03-09 23:44:01 +0700

    Small changes for adaptive
    Small changes for adaptive

--- JavaScript/menuMobile.js
+++ JavaScript/menuMobile.js
@@ -1,7 +1,14 @@
 
 const humburgerMenuBtn = document.querySelector('.humburger-menu')
 const mobileMenu = document.querySelector('.menu-mobile')
+const mobileMenuList = document.querySelectorAll('.menu-item')
 
 humburgerMenuBtn.addEventListener('click', ()=> {
     mobileMenu.classList.toggle('menu-open')
 }) 
+
+for (let item of mobileMenuList ) {
+    item.addEventListener('click', () => {
+        mobileMenu.classList.remove('menu-open')
+    })    
+}
